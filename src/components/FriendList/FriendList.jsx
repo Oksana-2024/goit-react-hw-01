c4867dbe5421f31a88a24f1fe669e24aa2bb5a5a
Oksana <oksanavasilevna77@gmail.com>
2024-12-07 15:54:03 +0200

    fix FriendList

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -4,12 +4,11 @@ import css from "./FriendList.module.css";
 export default function FriendList({ friends }) {
   return (
     <ul className={css.friendList}>
-      <li className={css.friendItem}>
-        {friends.map(({ id, ...friend }) => (
-          <FriendListItem key={id} {...friend} />
-        ))}
-      </li>
+      {friends.map(({ id, ...friend }) => (
+        <li key={id}>
+          <FriendListItem {...friend} />
+        </li>
+      ))}
     </ul>
   );
 }
-
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,4 +1,3 @@
-import clsx from "clsx";
 import css from "./FriendListItem.module.css";
 
 export default function FriendListItem({ avatar, name, isOnline }) {
@@ -6,7 +5,7 @@ export default function FriendListItem({ avatar, name, isOnline }) {
     <div className={css.friendCard}>
       <img className={css.friendAvatar} src={avatar} alt="Avatar" width="48" />
       <p className={css.friendName}>{name}</p>
-      <p className={clsx(isOnline ? css.friendOnline : css.friendOffline)}>
+      <p className={isOnline ? css.friendOnline : css.friendOffline}>
         {isOnline ? "Online" : "Offline"}
       </p>
     </div>
